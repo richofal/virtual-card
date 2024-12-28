4a970d59fd116ab52dbca8cf8697d44f2fb682fe
Richo Febrian <richofal25@gmail.com>
2024-12-28 19:47:52 +0700

    Fixing

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,7 +9,7 @@ function Card() {
       <div className="max-w-[300px] m-[2rem] bg-[#fffff9] rounded-[1rem] shadow-[0_30px_20px_-10px_rgba(0,_0,_0,_0.2)]">
         <div
           className="relative w-[300px] h-[360px] bg-white rounded-tl-[1rem] rounded-tr-[1rem] rounded-bl-[0] 
-          rounded-br-[0] bg-[url('/public/foto.jpg')] bg-no-repeat bg-top transition-[background-size] duration-[1500ms] 
+          rounded-br-[0] bg-[url('/foto.jpg')] bg-no-repeat bg-top transition-[background-size] duration-[1500ms] 
           ease-in-out bg-[length:100%]"
         >
           <Header />
